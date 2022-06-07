76cb2ae91858c638f088bc5bb9c8dc46bf9a17ee
FrancisOkolo <francischinedu78@gmail.com>
2022-06-07 13:56:37 -0700

    0th task

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-int_putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
-int_abs(int);
+int _abs(int);
 int print_last_digit(int);
 int add(int, int);
 void print_to_98(int n);
