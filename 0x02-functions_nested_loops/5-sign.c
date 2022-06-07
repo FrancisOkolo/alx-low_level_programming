a9eb1bc367f5cb16bd67e51351610fce56560fef
FrancisOkolo <francischinedu78@gmail.com>
2022-06-07 16:26:54 -0700

    5th task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - function that prints the sign of number
+ * @n: character to be verified
+ *
+ * Return: return 0 or 1
+ */
+int print_sign(int n)
+{
+	if (n > o)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == o)
+	{
+		_putchar('0');
+
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+
+		return (-1);
+	}
+}
+
