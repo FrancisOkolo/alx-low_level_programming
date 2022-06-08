9c85e66ab7a6865f33c835cecce69eb2f6b13fdc
FrancisOkolo <francischinedu78@gmail.com>
2022-06-07 18:13:44 -0700

    9th task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * times_table - prints up to 9 times table from 0
+ *
+ */
+void times_table(void)
+{
+	int row;
+	int column;
+	int value;
+
+	for (row 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			value = (row * column);
+
+			if (column == 0)
+			{
+				_putchar('0' + value);
+			}
+			else if (value <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + value);
+			}
+			else if (value > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (value / 10));
+				_putchar('0' + (value % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
