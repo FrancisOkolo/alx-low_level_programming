1156a0d95ac22dfc06c5022ed2437a8668697477
FrancisOkolo <francischinedu78@gmail.com>
2022-06-08 00:53:43 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,7 +11,7 @@ int print_last_digit(int);
 int add(int, int);
 void print_to_98(int n);
 void jack_bauer(void);
-void times_table(int n);
+void times_table(void);
 void print_times_table(int n);
 void print_times_table(int n);
 #endif
