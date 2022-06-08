0862f34cd2f465a584594b0ceb9d5e605d852e47
FrancisOkolo <francischinedu78@gmail.com>
2022-06-07 17:40:30 -0700

    8th task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,14 +14,14 @@ void jack_bauer(void)
 		{
 			h1 = i / 10 + '0';
 			h2 = i / 10 + '0';
-			mi = j / 10 + '0';
+			m1 = j / 10 + '0';
 			m2 = j / 10 + '0';
 			_putchar(h1);
 			_putchar(h2);
 			_putchar(':');
 			_putchar(m1);
 			_putchar(m2);
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
