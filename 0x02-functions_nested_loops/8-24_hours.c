31e57dfd668c13df453193ba588524da9db07172
FrancisOkolo <francischinedu78@gmail.com>
2022-06-07 17:37:15 -0700

    8th task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minutee of the day with 00:00 format
+ *
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int i, j, h1, h2, m1, m2;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			h1 = i / 10 + '0';
+			h2 = i / 10 + '0';
+			mi = j / 10 + '0';
+			m2 = j / 10 + '0';
+			_putchar(h1);
+			_putchar(h2);
+			_putchar(':');
+			_putchar(m1);
+			_putchar(m2);
+			_putchar('\n')
+		}
+	}
+}
