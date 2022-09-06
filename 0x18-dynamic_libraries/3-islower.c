e9b57e6dfc7085f3befea0dd0a340868e53eed7c
FrancisOkolo <francischinedu78@gmail.com>
2022-09-05 17:38:38 -0700

    Task 0

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - function that returns true for lower case number
+ * @c: char type letter
+ *
+ * Return: returns 0 on Succes and 1 on fail
+ */
+int _islower(int c)
+{
+	int i = islower(c);
+
+	if (i > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
